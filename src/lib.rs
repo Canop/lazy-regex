c34e1c902fbd56ecf14f0c9a8b80f3e939e3ff9b
Canop <cano.petrole@gmail.com>
2020-05-18 17:27:04 +0200

    import regex::Regex inside the macro call
    so that it's not necessary to do it outside

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! regex {
     ($s: literal) => {{
+        use regex::Regex;
         lazy_static! {
             static ref RE: Regex = Regex::new($s).unwrap();
         }
