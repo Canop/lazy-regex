99dda685880fde8de31084a9b646a399d6d51dec
Canop <cano.petrole@gmail.com>
2020-07-10 08:38:19 +0200

    add lib level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+/*!
+
+This crate introduces the `regex!` macro which is a shortcut to write static lazily compiled regular expressions as is usually done with lazy_static or once_cell.
+
+It lets you replace
+
+
+```not-executable
+fn some_helper_function(text: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new("...").unwrap();
+    }
+    RE.is_match(text)
+}
+
+```
+
+with
+
+
+```not-executable
+fn some_helper_function(text: &str) -> bool {
+    regex!("...").is_match(text)
+}
+```
+
+*/
+
 #[macro_export]
 macro_rules! regex {
     ($s: literal) => {{
